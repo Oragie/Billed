1c3b1a49d7a276908b97f5aed6a6a3fdc2fa11c7
Vinko <vinkoloic@gmail.com>
2025-01-16 15:45:50 +0100

    fix missing queries

--- frontend/src/containers/Bills.js
+++ frontend/src/containers/Bills.js
@@ -1,5 +1,5 @@
-import { formatDate, formatStatus } from "../app/format.js";
 import { ROUTES_PATH } from "../constants/routes.js";
+import { formatDate, formatStatus } from "../app/format.js";
 import Logout from "./Logout.js";
 
 export default class {
@@ -36,27 +36,19 @@ export default class {
   };
 
   getBills = () => {
-    // On vérifie si 'store' existe
     if (this.store) {
-      // On récupère la liste des factures depuis le store
       return this.store
-        .bills() // On accède à la section 'bills' du store
-        .list() // On appelle la méthode 'list' pour obtenir toutes les factures
+        .bills()
+        .list()
         .then((snapshot) => {
-          // On trie les factures par date décroissante (de la plus récente à la plus ancienne)
           const bills = snapshot
-          .sort((a, b) => {
-            const dateA = new Date(a.date); // Utilise le constructeur Date pour créer des objets Date
-            const dateB = new Date(b.date);
-            return dateB - dateA; // Compare directement les objets Date
-          })
-            // On reformate chaque facture en modifiant la date et le statut
+            .sort((a, b) => new Date(b.date) - new Date(a.date)) // tri date décroissant // BugFixé
             .map((doc) => {
               try {
                 return {
-                  ...doc, // On conserve les autres propriétés de la facture
-                  date: formatDate(doc.date), // On reformate la date de la facture
-                  status: formatStatus(doc.status), // On reformate le statut de la facture
+                  ...doc,
+                  date: formatDate(doc.date),
+                  status: formatStatus(doc.status),
                 };
               } catch (e) {
                 // if for some reason, corrupted data was introduced, we manage here failing formatDate function
@@ -70,7 +62,6 @@ export default class {
               }
             });
           console.log("length", bills.length);
-          console.log("bills", bills);
           return bills;
         });
     }
